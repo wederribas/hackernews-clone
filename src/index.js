d8a1d7c66da9ff171bdb437d5e8ba22ebb736aee
wederribas <wederribas@gmail.com>
2018-07-04 18:38:53 -0300

    Implement Login component with JWT auth and GraphQL mutations

--- src/index.js
+++ src/index.js
@@ -8,11 +8,26 @@ import { ApolloClient } from "apollo-client";
 import { HttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ApolloLink } from "apollo-link";
+import { AUTH_TOKEN } from "./constants";
 
 const httpLink = new HttpLink({ uri: "http://localhost:4000" });
 
+const middlewareAuthLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  const authorizationHeader = token ? `Bearer ${token}` : null;
+  operation.setContext({
+    headers: {
+      authorization: authorizationHeader
+    }
+  });
+  return forward(operation);
+});
+
+const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink);
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: httpLinkWithAuthToken,
   cache: new InMemoryCache()
 });
 
